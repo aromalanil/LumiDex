929d3cd8351461df32f12bda7013a7caa6c6bc73
Aromal Anil <aromalanilkannan@gmail.com>
2020-01-25 06:57:54 +0530

    nav bar responsive

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(()=>{
+    $('#menu').click(()=>{
+        $('.nav-div').toggleClass('nav-div-visible')
+
+        let img= $('.menu-icon').attr('src');
+
+        $('.menu-icon').attr('src',img==='./img/menu.svg' ? './img/cross.svg' : './img/menu.svg');
+        
+        
+    if($('.nav-div').hasClass('nav-div-visible'))
+    {
+        $('.nav-link').click(()=>{
+            $('.nav-div').removeClass('nav-div-visible')
+        })
+    }
+    })
+})

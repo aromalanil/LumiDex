b0aa4f9eaec0ba346451be6637b56a1487fd36ca
Aromal Anil <aromalanilkannan@gmail.com>
2020-01-26 12:09:23 +0530

    Version 1.0

--- script.js
+++ script.js
@@ -1,46 +1,41 @@
+/**
+ * @author Aromal Anil <github.com/aromalanil>
+ */
+
 $(document).ready(()=>
 {
-
-
-    // Redirect logo click to homepage
-    $('.logo').click(()=>
-    {
-        window.location.href = "./";
-    })
-
-
-
-
 
     // Menu bar for Mobile
 
     $('#menu').click(()=>{
+
+        //Making Side Menu Bar Visible/Invisible On Menu Click
         $('.nav-div').toggleClass('nav-div-visible');
 
-        let img= $('.menu-icon').attr('src');
 
+        //Changing Menu Icon
+        let img= $('.menu-icon').attr('src');
         $('.menu-icon').attr('src',img==='./img/menu.svg' ? './img/cross.svg' : './img/menu.svg');
         
         
-    if($('.nav-div').hasClass('nav-div-visible'))
-    {
-        $('.nav-link').click(()=>{
-            $('.nav-div').removeClass('nav-div-visible');
-            $('.menu-icon').attr('src','./img/menu.svg');
-        })
-    }
+        //Making Side Menu Bar Visible/Invisible When Clicked On Link
+        if($('.nav-div').hasClass('nav-div-visible'))
+        {
+            $('.nav-link').click(()=>{
+                $('.nav-div').removeClass('nav-div-visible');
+                $('.menu-icon').attr('src','./img/menu.svg');
+            })
+        }
     })
 
 
 
     //Testimonial Slider
     $('.slider').slick({
+
         nextArrow: '<i class="fa fa-chevron-right slick-next"></i>',
         prevArrow: '<i class="fa fa-chevron-left slick-prev"></i>',    
          
     });
-})
 
-// function load_home() {
-//     document.getElementById("content").innerHTML='<object type="text/html" data="home.html" ></object>';
-// }
+})

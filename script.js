abd2a46c0ef39f9143236d767c938a5a383f5080
Aromal Anil <aromalanilkannan@gmail.com>
2020-01-26 19:37:21 +0530

    added a custom page loader

--- script.js
+++ script.js
@@ -2,28 +2,33 @@
  * @author Aromal Anil <github.com/aromalanil>
  */
 
-$(document).ready(()=>
-{
+
+$(document).ready(() => {
+
+    //To remove Loader on page load
+    $(window).on('load', () => {
+        $('.loader').fadeOut('fast');
+    })
+
+
 
     // Menu bar for Mobile
-
-    $('#menu').click(()=>{
+    $('#menu').click(() => {
 
         //Making Side Menu Bar Visible/Invisible On Menu Click
         $('.nav-div').toggleClass('nav-div-visible');
 
 
         //Changing Menu Icon
-        let img= $('.menu-icon').attr('src');
-        $('.menu-icon').attr('src',img==='./img/icons/menu.svg' ? './img/icons/cross.svg' : './img/icons/menu.svg');
-        
-        
+        let img = $('.menu-icon').attr('src');
+        $('.menu-icon').attr('src', img === './img/icons/menu.svg' ? './img/icons/cross.svg' : './img/icons/menu.svg');
+
+
         //Making Side Menu Bar Visible/Invisible When Clicked On Link
-        if($('.nav-div').hasClass('nav-div-visible'))
-        {
-            $('.nav-link').click(()=>{
+        if ($('.nav-div').hasClass('nav-div-visible')) {
+            $('.nav-link').click(() => {
                 $('.nav-div').removeClass('nav-div-visible');
-                $('.menu-icon').attr('src','./img/icons/menu.svg');
+                $('.menu-icon').attr('src', './img/icons/menu.svg');
             })
         }
     })
@@ -36,7 +41,7 @@ $(document).ready(()=>
         nextArrow: '<i class="fa fa-chevron-right slick-next"></i>',
         prevArrow: '<i class="fa fa-chevron-left slick-prev"></i>',
         autoplay: true,
-        autoplaySpeed:7000,
+        autoplaySpeed: 7000,
     });
 
 })
